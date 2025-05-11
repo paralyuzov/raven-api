e9161aeb4657808c4a8a721ad451ac9cdab23a09
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-11 14:56:23 +0300

    implement create message functionality

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
+import { Message } from './schemas/message.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { FriendsService } from 'src/friends/friends.service';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  constructor(
+    @InjectModel(Message.name) private messageModel: Model<Message>,
+    private readonly friendsService: FriendsService,
+  ) {}
+
+  async createMessage(
+    senderId: string,
+    receiverId: string,
+    content: string,
+    type: string = 'text',
+  ): Promise<Message> {
+    const areFriends = await this.friendsService.areFriends(
+      senderId,
+      receiverId,
+    );
+
+    if (!areFriends) {
+      throw new ForbiddenException('You can only send messages to friends');
+    }
+
+    return this.messageModel.create({
+      senderId,
+      receiverId,
+      content,
+      type,
+    });
+  }
+}
