47b62db3727c8b08d4edacfd9af12176db150c5a
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-11 14:57:04 +0300

    add POST endpoint to  create message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/decorators/get-user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CreateMessageDto } from './dto/create.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
+@UseGuards(AuthGuard)
 export class MessagesController {
+  constructor(private readonly messageService: MessagesService) {}
+
   @Post()
-  getMessages() {
-    return 'This action returns all messages';
+  async createMessage(
+    @GetUser('_id') userId: string,
+    @Body() messageDto: CreateMessageDto,
+  ) {
+    return this.messageService.createMessage(
+      userId,
+      messageDto.receiverId,
+      messageDto.content,
+      messageDto.type,
+    );
   }
 }
