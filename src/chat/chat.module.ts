2719243286da8bf71f260603cd710c8df8f15157
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-16 16:04:36 +0300

    import friendsModule and WsAuthGuard

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -4,12 +4,15 @@ import { ChatGateway } from './chat.gateway';
 import { Message, MessageSchema } from '../messages/schemas/message.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessagesModule } from '../messages/messages.module';
+import { WsAuthGuard } from 'src/guards/ws-auth.guard';
+import { FriendsModule } from 'src/friends/friends.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
     MessagesModule,
+    FriendsModule,
   ],
-  providers: [ChatGateway, ChatService],
+  providers: [ChatGateway, ChatService, WsAuthGuard],
 })
 export class ChatModule {}
