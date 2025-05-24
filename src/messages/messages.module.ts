80d2db9b50651405214e6019513b9be3a531c9e3
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-24 12:09:23 +0300

    import Conversation Schema

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { Message, MessageSchema } from './schemas/message.schema';
+import {
+  Conversation,
+  ConversationSchema,
+} from '../conversation/schemas/conversation.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FriendsModule } from 'src/friends/friends.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    MongooseModule.forFeature([
+      { name: Message.name, schema: MessageSchema },
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
     FriendsModule,
   ],
   controllers: [MessagesController],
