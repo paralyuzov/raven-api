b346d0b42048059a16e1af19e6abc546ad24499d
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-24 12:08:08 +0300

    import conversation schema

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -6,12 +6,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MessagesModule } from '../messages/messages.module';
 import { WsAuthGuard } from 'src/guards/ws-auth.guard';
 import { FriendsModule } from 'src/friends/friends.module';
+import { ConversationModule } from 'src/conversation/conversation.module';
+import {
+  Conversation,
+  ConversationSchema,
+} from '../conversation/schemas/conversation.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    MongooseModule.forFeature([
+      { name: Message.name, schema: MessageSchema },
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
     MessagesModule,
     FriendsModule,
+    ConversationModule,
   ],
   providers: [ChatGateway, ChatService, WsAuthGuard],
 })
