8a624245708b497e9f52dfcc3a5979d53d164b33
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-24 12:06:24 +0300

    Update message.schema.ts

--- src/messages/schemas/message.schema.ts
+++ src/messages/schemas/message.schema.ts
@@ -1,21 +1,38 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 
+export enum MessageType {
+  TEXT = 'text',
+  IMAGE = 'image',
+  VIDEO = 'video',
+  GIF = 'gif',
+}
+
 @Schema({ timestamps: true })
 export class Message extends Document {
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
-  senderId: mongoose.Schema.Types.ObjectId;
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true,
+  })
+  conversationId: mongoose.Schema.Types.ObjectId;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
-  receiverId: mongoose.Schema.Types.ObjectId;
+  senderId: mongoose.Schema.Types.ObjectId;
 
   @Prop({ required: true })
   content: string;
 
-  @Prop({ required: true, enum: ['text', 'image', 'video', 'gif'] })
-  type: string;
-
   @Prop({ default: false })
   read: boolean;
+
+  @Prop({ type: String, enum: MessageType, default: MessageType.TEXT })
+  type: MessageType;
+
+  @Prop()
+  createdAt?: Date;
+
+  @Prop()
+  updatedAt?: Date;
 }
 export const MessageSchema = SchemaFactory.createForClass(Message);
