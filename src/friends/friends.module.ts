ad0b14d40fa79cae57d09c092dec430d8c805150
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-05-25 10:10:59 +0300

    import message and conversation schemas

--- src/friends/friends.module.ts
+++ src/friends/friends.module.ts
@@ -4,11 +4,20 @@ import { FriendsController } from './friends.controller';
 import { Friend, FriendSchema } from './schemas/friend.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/auth/schemas/user.schema';
+import { Message, MessageSchema } from 'src/messages/schemas/message.schema';
+import {
+  Conversation,
+  ConversationSchema,
+} from 'src/conversation/schemas/conversation.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Friend.name, schema: FriendSchema }]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    MongooseModule.forFeature([
+      { name: Conversation.name, schema: ConversationSchema },
+    ]),
   ],
   controllers: [FriendsController],
   providers: [FriendsService],
